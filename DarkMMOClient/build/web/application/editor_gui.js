fc11886dc06d8938048b2f3b8adc72fea18e1fac
cyberqat <jeffpk@worldwizards.net>
2010-01-31 15:12:15 -0800

    Download and display of tielset names working
    Signed-off-by: cyberqat <jeffpk@worldwizards.net>

--- DarkMMOClient/build/web/application/editor_gui.js
+++ DarkMMOClient/build/web/application/editor_gui.js
@@ -2,100 +2,122 @@
  * This file builds the GUI for the DarkMMO world editor
  * @author jeff
  */
-function makeGui(){
+
+
+(function() {
+    var _tileSetStore= new Ext.data.SimpleStore({
+        data:[['foo'],['bar']],
+        fields:['tileset']
+    })
     var _tilesStore = new Ext.data.SimpleStore({
         data: [['foo'],['bar']],
         fields: ['tilename']
     });
-    var viewport = new Ext.Viewport({
-        layout: 'border',
-        renderTo: Ext.getBody(),
-        items: [{
-            region: 'north',
-            xtype: 'toolbar',
-            items: [{
-                xtype: 'tbbutton',
-                text: 'File',
-                menu: [{
-                    text: 'Load'
-                }, {
-                    text: 'Save'
-                }]
-            }]
-        }, {
-            region: 'south',
-            xtype: 'panel',
-            html: 'South'
-        }, {
-            region: 'east',
-            layout: 'border',
-            title: 'Tile Sets',
-            split: true,
-            items: [{
-                region: 'north',
-                xtype: 'combo',
-                name: 'tileSetSelector',
-                mode: 'local',
-                store: [],
-                height: 100,
-                id: 'tileSetCombo'
-            }, {
-                region: 'center',
-                layout: 'accordion',
+    GUI = {
+        setTileSets: function(array){
+            array.sort();
+            var arrayArray = []
+            for (var i=0;i<array.length;i++){
+                arrayArray.push([array[i]])
+            }
+            _tileSetStore.loadData(arrayArray)
+        },
+        makeGui: function(){
+   
+            var viewport = new Ext.Viewport({
+                layout: 'border',
+                renderTo: Ext.getBody(),
                 items: [{
-                    title: 'Features',
-                    items: [{
-                        xtype: 'grid',
-                        id:'featuresGrid',
-                        store: _tilesStore,
-                        autoHeight: true,
-                        autoWidth: true,
-                        columns: [{
-                            header: 'Tile Name',
-                            dataIndex: 'tilename'
-                        }]
+                        region: 'north',
+                        xtype: 'toolbar',
+                        items: [{
+                                xtype: 'tbbutton',
+                                text: 'File',
+                                menu: [{
+                                        text: 'Load'
+                                    }, {
+                                        text: 'Save'
+                                    }]
+                            }]
+                    }, {
+                        region: 'south',
+                        xtype: 'panel',
+                        html: 'South'
+                    }, {
+                        region: 'east',
+                        layout: 'border',
+                        title: 'Tile Sets',
+                        width: 200,
+                        items: [{
+                                region: 'north',
+                                xtype: 'combo',
+                                name: 'tileSetSelector',
+                                editable: false,
+                                clearFilterOnReset: true,
+                                triggerAction: 'all',
+                                lastQuery: '', // == allQuery
+                                mode: 'local',
+                                store: _tileSetStore,
+                                displayField: 'tileset',
+                                height: 100,
+                                id: 'tileSetCombo'
+                            }, {
+                                region: 'center',
+                                layout: 'accordion',
+                                items: [{
+                                        title: 'Features',
+                                        items: [{
+                                                xtype: 'grid',
+                                                id:'featuresGrid',
+                                                store: _tilesStore,
+                                                autoHeight: true,
+                                                autoWidth: true,
+                                                columns: [{
+                                                        header: 'Tile Name',
+                                                        dataIndex: 'tilename'
+                                                    }]
+                                            }]
+                                    }, {
+                                        title: 'Groups',
+                                        items: [{
+                                                xtype: 'grid',
+                                                id:'groupsGrid',
+                                                store: _tilesStore,
+                                                autoHeight: true,
+                                                autoWidth: true,
+                                                columns: [{
+                                                        header: 'Tile Name',
+                                                        dataIndex: 'tilename'
+                                                    }]
+                                            }]
+                                    }, {
+                                        title: 'Terrain',
+                                        items: [{
+                                                xtype: 'grid',
+                                                id:'terrainGrid',
+                                                store: _tilesStore,
+                                                autoHeight: true,
+                                                autoWidth: true,
+                                                columns: [{
+                                                        header: 'Tile Name',
+                                                        dataIndex: 'tilename'
+                                                    }]
+                                            }]
+                                    }]
+                            }]
+                    }, {
+                        region: 'west',
+                        xtype: 'panel',
+                        html: 'West'
+                    }, {
+                        region: 'center',
+                        xtype: 'panel',
+                        html: 'Center'
                     }]
-                }, {
-                    title: 'Groups',
-                    items: [{
-                        xtype: 'grid',
-                        id:'groupsGrid',
-                        store: _tilesStore,
-                        autoHeight: true,
-                        autoWidth: true,
-                        columns: [{
-                            header: 'Tile Name',
-                            dataIndex: 'tilename'
-                        }]
-                    }]
-                }, {
-                    title: 'Terrain',
-                    items: [{
-                        xtype: 'grid',
-                        id:'terrainGrid',
-                        store: _tilesStore,
-                        autoHeight: true,
-                        autoWidth: true,
-                        columns: [{
-                            header: 'Tile Name',
-                            dataIndex: 'tilename'
-                        }]
-                    }]
-                }]
-            }]
-        }, {
-            region: 'west',
-            xtype: 'panel',
-            html: 'West'
-        }, {
-            region: 'center',
-            xtype: 'panel',
-            html: 'Center'
-        }]
-    });
-    console.log(viewport)
-}
+            });
+            console.log(viewport)
+        }
+    }
+})()
 
-function setTileSets(array){
-    Ext.get('tileSetCombo').store=array
-}
+
